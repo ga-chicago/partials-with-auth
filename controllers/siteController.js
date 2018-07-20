b16a50c7295f210e4def6a43d848b6cbd35ad231
Reuben Ayres <ra@sent.com>
2018-07-20 16:54:23 -0500

    'you must be logged in to do that' with styling for messages

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+  if(!req.session.loggedIn) {
+    req.session.message = "You must be logged in to do that.", 
+    req.session.messageClass = "bad",
+    res.redirect('/user/login')
+  }
+  else {
+    next()
+  }
+})
+
 router.get('/', (req, res) => {
   res.redirect('/site/0');
 })
 
 router.get('/:id', (req, res) => {
   const message = req.session.message;
+  const messageClass = req.session.messageClass;
   req.session.message = null
   res.render('home.ejs', { 
     theNumber: req.params.id, 
-    message: message
+    message: message,
+    messageClass: messageClass
   })
 })
 
